749ddfaf50743388a692da969399d9ad916e855d
Marcus Brorsson <marcusbrorsson@hotmail.com>
2017-10-25 14:10:17 +0200

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  def hello
+    render html: "¡Hola, mundo!!"
+
+  end
+def goodbye
+
+  render html: "Good Bye Suckers"
+end
+
+
+end
